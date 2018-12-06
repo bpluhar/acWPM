47251aee1bcc9afa3ad5c6936369ec5988600135
Brian <bpluhar@fuselenses.com>
2018-12-06 16:29:18 -0500

    Initial Commit

---
+++ WindowsFormsApp4/Main.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMaxAmmo = new System.Windows.Forms.Button();
+            this.btnReadMemory = new System.Windows.Forms.Button();
+            this.lblProcessId = new System.Windows.Forms.Label();
+            this.btnMaxHealth = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnMaxAmmo
+            // 
+            this.btnMaxAmmo.Location = new System.Drawing.Point(134, 41);
+            this.btnMaxAmmo.Name = "btnMaxAmmo";
+            this.btnMaxAmmo.Size = new System.Drawing.Size(120, 23);
+            this.btnMaxAmmo.TabIndex = 0;
+            this.btnMaxAmmo.Text = "1000 Ammo";
+            this.btnMaxAmmo.UseVisualStyleBackColor = true;
+            this.btnMaxAmmo.Click += new System.EventHandler(this.btnMaxAmmo_Click);
+            // 
+            // btnReadMemory
+            // 
+            this.btnReadMemory.Location = new System.Drawing.Point(12, 12);
+            this.btnReadMemory.Name = "btnReadMemory";
+            this.btnReadMemory.Size = new System.Drawing.Size(242, 23);
+            this.btnReadMemory.TabIndex = 1;
+            this.btnReadMemory.Text = "Attach";
+            this.btnReadMemory.UseVisualStyleBackColor = true;
+            this.btnReadMemory.Click += new System.EventHandler(this.btnReadMemory_Click);
+            // 
+            // lblProcessId
+            // 
+            this.lblProcessId.AutoSize = true;
+            this.lblProcessId.Location = new System.Drawing.Point(13, 101);
+            this.lblProcessId.Name = "lblProcessId";
+            this.lblProcessId.Size = new System.Drawing.Size(0, 13);
+            this.lblProcessId.TabIndex = 2;
+            // 
+            // btnMaxHealth
+            // 
+            this.btnMaxHealth.Location = new System.Drawing.Point(12, 41);
+            this.btnMaxHealth.Name = "btnMaxHealth";
+            this.btnMaxHealth.Size = new System.Drawing.Size(116, 23);
+            this.btnMaxHealth.TabIndex = 3;
+            this.btnMaxHealth.Text = "1000 Health";
+            this.btnMaxHealth.UseVisualStyleBackColor = true;
+            this.btnMaxHealth.Click += new System.EventHandler(this.btnMaxHealth_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(266, 126);
+            this.Controls.Add(this.btnMaxHealth);
+            this.Controls.Add(this.lblProcessId);
+            this.Controls.Add(this.btnReadMemory);
+            this.Controls.Add(this.btnMaxAmmo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AssaultCube Trainer";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMaxAmmo;
+        private System.Windows.Forms.Button btnReadMemory;
+        private System.Windows.Forms.Label lblProcessId;
+        private System.Windows.Forms.Button btnMaxHealth;
+    }
+}
+
---
+++ WindowsFormsApp4/Main.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp4.Utilities;
+
+namespace WindowsFormsApp4
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+        // Ammo Offsets
+        // The start of the ammo value in memory
+        uint pAmmoAddress = 0x0010F4F4;
+        // The offset from the start of the pAddress to the stored Ammo value(0x0150 = 336)
+        int pAmmoOffset = 0x0150;
+        // Becomes address where ammo is stored
+
+        // Health Offsets
+        // The start of the health value in memory
+        uint pHealthAddress = 0x00109B74;
+        // The offset from the start of the pHealthAddress to the stored Health value ()
+        int pHealthOffset = 0x00F8;
+
+        uint ammooffset = 0;
+        uint healthoffset = 0;
+        int bytesOut = 0;
+
+        ProcessMemoryReader mReader = new ProcessMemoryReader();
+
+        private void btnReadMemory_Click(object sender, EventArgs e)
+        {
+            
+            Process p = Process.GetProcessesByName("ac_client").ToList().FirstOrDefault();
+            if (p != null)
+            {
+                mReader.ReadProcess = p;
+                lblProcessId.Text = String.Format("Process ID: {0:G}", p.Id);
+                mReader.OpenProcess();
+
+                // Ammo Reading
+                ammooffset = BitConverter.ToUInt32(mReader.ReadMemory((IntPtr)(pAmmoAddress + (uint)p.Modules[0].BaseAddress), 4, out bytesOut), 0);
+                if (pAmmoOffset < 0)
+                {
+                    ammooffset -= (uint)Math.Abs(pAmmoOffset);
+                }
+                else
+                {
+                    ammooffset += (uint)pAmmoOffset;
+                }
+
+                healthoffset = BitConverter.ToUInt32(mReader.ReadMemory((IntPtr)(pHealthAddress + (uint)p.Modules[0].BaseAddress), 4, out bytesOut), 0);
+                if (pHealthOffset < 0)
+                {
+                    healthoffset -= (uint)Math.Abs(pHealthOffset);
+                }
+                else
+                {
+                    healthoffset += (uint)pHealthOffset;
+                }
+
+                btnReadMemory.Enabled = false;
+                btnReadMemory.Text = "Attached!";
+                btnMaxAmmo.Enabled = true;
+                btnMaxHealth.Enabled = true;
+
+            }
+            else if (p == null)
+            {
+                lblProcessId.Text = "Process not found! Is it running?";
+            }
+        }
+
+        private void btnMaxAmmo_Click(object sender, EventArgs e)
+        {
+            mReader.WriteMemory((IntPtr)ammooffset, BitConverter.GetBytes(1000), out bytesOut);
+
+        }
+
+        private void btnMaxHealth_Click(object sender, EventArgs e)
+        {
+            mReader.WriteMemory((IntPtr)healthoffset, BitConverter.GetBytes(1000), out bytesOut);
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            lblProcessId.Text = "Not attached";
+            btnMaxAmmo.Enabled = false;
+            btnMaxHealth.Enabled = false;
+        }
+    }
+}
